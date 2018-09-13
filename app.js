9817c52a13510b73b5af82782b02ba1d40c17ff9
Patrick Biffle <pbiffle@gmail.com>
2018-09-13 13:33:04 -0600

    readying for push to heroku

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 const queries = require('./queries')
 
 app.use(bodyParser.json())
+app.use(cors())
 
 app.listen(port, () => {
     console.log(`listening on ${port}`)
